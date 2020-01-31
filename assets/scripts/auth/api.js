12c2079dbbc104484887fa5eb3dd4584f23064b7
Avery Burne <burneav@gmail.com>
2020-01-31 17:06:10 -0500

    Nothing

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -46,11 +46,11 @@ const getGames = function (event) {
   })
 }
 
-const addGame = function () {
-  return $.ajax({
-    url: config.apiUrl +
-  })
-}
+// const addGame = function () {
+//   return $.ajax({
+//     url: config.apiUrl +
+//   })
+// }
 
 const updateBoard = function (event) {
   return $.ajax({
